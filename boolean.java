0014b1e63136a0b47b1f3fca03fb55c288e28f6b
Codemite <142070275+Codemite444@users.noreply.github.com>
2023-09-04 17:36:23 +0800

    Create Raposala boolean.java

---
+++ boolean.java
@@ -0,0 +1,12 @@
+//boolean
+public class Demo {
+    
+    public static void main(String[] args) {
+        
+        boolean t = true;
+        System.out.println(t);
+        
+        boolean f = false;
+        System.out.println(f);
+    }
+}
