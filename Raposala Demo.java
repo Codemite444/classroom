bbb7e97b17a325674fa5f514185a886a5a65fec3
Codemite <142070275+Codemite444@users.noreply.github.com>
2023-09-04 17:37:07 +0800

    Create Raposala Demo.java

---
+++ Raposala Demo.java
@@ -0,0 +1,25 @@
+public class Demo {
+    
+    public static void main(String[] args)
+{
+       // float type
+      float f = 20.25f;
+      System.out.println("f=" + f);
+      
+      // hi ako si janlloyd
+      double d = 20.25d;
+      System.out.println("d=" + d);
+  
+      char ch = 'S';
+      System.out.println(ch);
+      
+      char ch2 = '&';
+      System.out.println(ch2);
+      
+      char ch3 = '$';
+      System.out.println(ch3);
+ 
+      char ch4 = '^';
+      System.out.println(ch4);
+   }
+}
