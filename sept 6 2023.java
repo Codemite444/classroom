bb21aa891738d87f4c504f190e8de977b5137173
Codemite <142070275+Codemite444@users.noreply.github.com>
2023-09-06 01:49:29 -0700

    Create sept 6 2023.java

---
+++ sept 6 2023.java
@@ -0,0 +1,10 @@
+class name {
+    public static void main(String[] args) {
+        
+        
+        int x = 10, y = 20, z = 3;
+        int yyy = x+y+z;
+        
+        System.out.println("The sum of x, y and z is: " + yyy );
+    }
+}
