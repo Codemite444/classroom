a2c8d4a5b199d8a2d5d6de32c882991d5e9c1f5b
Codemite <142070275+Codemite444@users.noreply.github.com>
2023-09-06 02:23:00 -0700

    Create sept 6 2023 b.java

---
+++ sept 6 2023 b.java
@@ -0,0 +1,12 @@
+class name {
+    public static void main(String[] args) {
+        
+       String name = "Janlloyd Raposala";
+       int Age = 19, weeks1 = 4, weeks2 = 13;
+       int totalweeks = weeks1*weeks2;
+       
+       System.out.println("My name is " + name);
+       System.out.println("I am " + Age);
+       System.out.println("Total number of weeks in a year is " + totalweeks);
+    }
+}  
