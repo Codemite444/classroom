7d455cf4e15e724a942b4b7160e8c04cc1b9c0a5
Codemite <142070275+Codemite444@users.noreply.github.com>
2023-09-11 19:58:03 +0800

    Create sept 11 2023 a.java

---
+++ sept 11 2023 a.java
@@ -0,0 +1,17 @@
+class Calculator1 {
+    public static void main(String[] args) {
+        
+        //September 11 2023
+        int x = 10, y = 15;
+        int sum = x + y;
+        int difference = x - y;
+        int quotient = x / y;
+        int product = x * y;
+
+        System.out.println("The sum of x and y is: " + sum);
+        System.out.println("The difference of x and y is: " + difference);
+        System.out.println("The quotient of x and y is: " + quotient);
+        System.out.println("The product of x and y is: " + product);
+
+    }
+}
